71efc0a7988cf0ee9b95bcbf3641d8554574218f
Brand98 <brahyanbedoya@hotmail.es>
2019-05-27 02:26:20 -0500

    El crud se encuentra listo, pero pendiente de mejoras

---
+++ FinalSeguimiento/FinalSeguimiento/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using FinalSeguimiento.Models;
+using System.Text.RegularExpressions;
+using SQLite;
+
+namespace FinalSeguimiento
+{
+    public partial class MainPage : ContentPage
+    {      
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Clicked(object sender, EventArgs e)
+        {            
+            string num = Conexion.Instancia.cont().ToString();
+            DisplayAlert("Alert", "numero de registros " + num, "OK");
+
+            var allMovimientos = Conexion.Instancia.GetAllReg();
+            RegistroMov.ItemsSource = allMovimientos;
+            Estado.Text = Conexion.Instancia.EstadoDeMensaje;
+        }
+
+        private void BtnAgregar_Clicked(object sender, EventArgs e)
+        {
+            Estado.Text = string.Empty;
+            Conexion.Instancia.addNew(mov.Text, con.Text, Convert.ToDouble(val.Text), det.Text, Convert.ToDateTime(fec.Text), rec.Text);
+            Estado.Text = Conexion.Instancia.EstadoDeMensaje;
+            DisplayAlert("Alert", Estado.Text, "OK");
+            Clean();             
+        }
+
+        private void BtnEliminarxNombre_Clicked(object sender, EventArgs e)
+        {
+            Conexion.Instancia.DeleteByName(con.Text);
+            DisplayAlert("Alert", "El registro fue eliminado con exito", "OK");
+        }
+
+        private void BtnEliminar_Clicked(object sender, EventArgs e)
+        {
+            Conexion.Instancia.DeleteByName(con.Text);
+            DisplayAlert("Alert", "Los registros se eliminaron con exito", "OK");            
+        }
+
+        private void BtnActualizar_Clicked(object sender, EventArgs e)
+        {
+            Conexion.Instancia.Update(mov.Text, con.Text, Convert.ToDouble(val.Text), det.Text, Convert.ToDateTime(fec.Text), rec.Text);
+        }
+
+        public void Clean()
+        {
+            mov.Text = string.Empty;
+            con.Text = string.Empty;
+            val.Text = string.Empty;
+            det.Text = string.Empty;
+            fec.Text = string.Empty;
+            rec.Text = string.Empty;
+        }
+
+        private async Task validarFormulario()
+        {
+            //Valida si el valor en el Entry se encuentra vacio o es igual a Null
+            if (String.IsNullOrWhiteSpace(mov.Text))
+            {
+                await this.DisplayAlert("Advertencia", "El campo del nombre es obligatorio.", "OK");
+               // return false;
+            }
+            //Valida que solo se ingresen letras
+            else if (!mov.Text.ToCharArray().All(Char.IsLetter))
+            {
+                await this.DisplayAlert("Advertencia", "Tu información contiene números, favor de validar.", "OK");
+                //return false;
+            }
+            else
+            {
+                //Valida si se ingresan caracteres especiales
+                string caractEspecial = @"^[^ ][a-zA-Z ]+[^ ]$";
+                bool resultado = Regex.IsMatch(mov.Text, caractEspecial, RegexOptions.IgnoreCase);
+                if (!resultado)
+                {
+                    await this.DisplayAlert("Advertencia", "No se aceptan caracteres especiales, intente de nuevo.", "OK");
+                  //  return false;
+                }
+            }
+        }        
+    }
+}
---
+++ FinalSeguimiento/FinalSeguimiento/Models/Conexion.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using SQLite;
+
+namespace FinalSeguimiento.Models
+{
+    public class Conexion
+    {
+        private SQLiteConnection con;
+
+        private static Conexion instancia;
+
+        public static Conexion Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                    throw new Exception("Debe llamar al inicializador");
+                return instancia;
+            }
+        }
+
+        public static void Inicializador(string filename)
+        {
+            if (filename == null)
+                throw new ArgumentNullException();
+            if (instancia != null)
+                instancia.con.Close();
+
+            instancia = new Conexion(filename);
+        }
+
+        private Conexion(string dbPath)
+        {
+            con = new SQLiteConnection(dbPath);
+            con.CreateTable<Registro>();
+        }
+
+        public string EstadoDeMensaje;
+
+        public int addNew(string mov, string cto, double val, string obs, DateTime fec, string rec)
+        {
+            int result = 0;
+            try
+            {
+                result = con.Insert(new Registro() {
+                    MOVIMIENTO = mov,
+                    CONCEPTO = cto,
+                    VALOR = Convert.ToDouble(val),
+                    OBSERVACION = obs,
+                    FECHA = Convert.ToDateTime(fec),
+                    RECURRENCIA = rec
+                });
+
+                EstadoDeMensaje = string.Format("Cantidad de filas afectadas: {0}", result);
+
+            } catch (Exception e)
+            {
+                EstadoDeMensaje = e.Message;
+            }
+            return result;
+        }
+
+        public IEnumerable<Registro> GetAllReg()
+        {
+            try
+            {
+                return con.Table<Registro>();
+            } catch (Exception e)
+            {
+                EstadoDeMensaje = e.Message;
+            }
+            return Enumerable.Empty<Registro>();
+        }   
+
+        public int cont()
+        {
+            int numRows = con.Table<Registro>().Count();
+            return numRows;
+        }
+
+        public IEnumerable<Registro> Delete()
+        {
+            con.DeleteAll<Registro>();            
+            return Enumerable.Empty<Registro>();
+        }
+
+        public IEnumerable<Registro> DeleteByName(string nombre)
+        {
+            con.Table<Registro>().Delete(p => p.CONCEPTO == nombre);
+            //return Conexion.instancia.con.Query<Registro>(query);
+            return Enumerable.Empty<Registro>();
+        }
+
+        public IEnumerable<Registro> Update(string movimiento, string nombre, double valor, string observacion, DateTime fecha, string recurrencia)
+        {
+            con.Table<Registro>().Select(p => p.CONCEPTO == nombre);
+            //var actualizar = con.Query<Registro>("SELECT * FROM Registro WHERE CONCEPTO = ?", nombre);
+            var actualizar = con.Query<Registro>("UPDATE Registro SET MOVIMIENTO = ?, VALOR = ?, OBSERVACION = ?, FECHA = ?, RECURRENCIA = ? WHERE CONCEPTO = ?", 
+                movimiento, valor, observacion, fecha, recurrencia, nombre);
+            foreach (var s in actualizar)
+            {
+
+                Console.WriteLine("a " + s.FECHA);
+            }
+            con.Table<Registro>().Delete(p => p.CONCEPTO == nombre);
+            //return Conexion.instancia.con.Query<Registro>(query);
+            return Enumerable.Empty<Registro>();
+        }
+    }
+}
